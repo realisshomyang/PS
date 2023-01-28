ea90801cd77cf3841311908b986a7b8b40401f79
unknown <realisshoman@kakao.com>
2023-01-28 23:31:05 +0900

    2023.01.28

--- 710- 그리디/11399.py
+++ 710- 그리디/11399.py
@@ -1,4 +1,9 @@
-N = int(input())
+n = int(input())
+peoples = list(map(int, input().split()))
 
+peoples.sort()
+answer = 0
 
-dddd
+for x in range(1, n+1):
+    answer += sum(peoples[0:x])
+print(answer)
