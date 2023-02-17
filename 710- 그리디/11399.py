01920ae3baa74666e73d691b3c18143bedd2e904
unknown <realisshoman@kakao.com>
2023-02-17 22:55:46 +0900

    dd

--- 710- 그리디/11399.py
+++ 710- 그리디/11399.py
@@ -1,9 +1,9 @@
 n = int(input())
-peoples = list(map(int, input().split()))
+people = list(map(int, input().split()))
 
-peoples.sort()
+people.sort()
 answer = 0
 
 for x in range(1, n+1):
-    answer += sum(peoples[0:x])
+    answer += sum(people[0:x])
 print(answer)
