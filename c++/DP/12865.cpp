8bffa6c5b9b777a8759c8d4e62d669751251b007
realisshomyang <realisshoman@kakao.com>
2023-03-27 21:11:27 +0900

    PS update

---
+++ c++/DP/12865.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+const int MAXN = 101;
+const int MAXK = 100001;
+int N, K;
+
+int main()
+{
+    cin >> N >> K;
+}
---
+++ c++/DP/9084.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int N;
+int coin[21];
+
+int topdown(int n)
+{
+    for (int i = 0; i < N; i++)
+    {
+    }
+}
+
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+    }
+}
